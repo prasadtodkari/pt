431daa8da5e3c309d5c71284dfa6baa7167b7ca4
Prasad Todkari <prasadtodkari@gmail.com>
2022-06-28 20:04:27 +0530

    updated

---
+++ Jun_27/index.js
@@ -0,0 +1,8 @@
+let num1 = 35;
+let num2 = 67;
+let num3 = 82;
+
+let max = Math.max(num1.value, num2.value, num3.value);
+
+console.log(max);
+
---
+++ Jun_28/index.js
@@ -0,0 +1,31 @@
+let num1 = 35;
+
+if (num1%2 == 0) {
+    console.log("Even Number")
+} else console.log("Odd Number");
+
+let num = 56;
+
+if (num%5 == 0 && num%3 == 0){
+    console.log("Fiz Buzz");
+}else if(num%3 == 0){
+    console.log(Fiz);
+}else if(num%5 == 0){
+    console.log("Buzz");
+}else {
+    console.log("this is just a Number")
+}
+
+let numb = 20;
+
+while( numb > 0){
+    console.log(numb);
+    numb = numb - 1;
+}
+
+let even = 2;
+
+while( even < 100){
+    console.log(even);
+    even = even + 2;
+}
