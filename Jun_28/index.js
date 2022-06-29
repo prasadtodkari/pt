d204392df7eae630b144ddc21fd1c8bb7a2ed833
Prasad Todkari <prasadtodkari@gmail.com>
2022-06-29 16:37:45 +0530

    Taxk completed

--- Jun_28/index.js
+++ Jun_28/index.js
@@ -1,9 +1,11 @@
+//Test 1 Odd Even Number
 let num1 = 35;
 
 if (num1%2 == 0) {
     console.log("Even Number")
-} else console.log("Odd Number");
+} else console.log("Odd Number"); 
 
+//Test 2 Fizz Buzz
 let num = 56;
 
 if (num%5 == 0 && num%3 == 0){
@@ -16,6 +18,7 @@ if (num%5 == 0 && num%3 == 0){
     console.log("this is just a Number")
 }
 
+//Test 3 While Loop revers
 let numb = 20;
 
 while( numb > 0){
@@ -23,9 +26,18 @@ while( numb > 0){
     numb = numb - 1;
 }
 
+//Test 4 Loop Even Number
 let even = 2;
 
 while( even < 100){
     console.log(even);
     even = even + 2;
+} 
+
+//Task of the day
+let prime = 34552;
+if (prime%2 == 0){
+    console.log("This is not a Prime Number")
+} else{
+    console.log("This is a Prime Number");
 }
