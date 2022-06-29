b2c062d25d22c6cdd7d9f6b1e0f5a801b849475c
Prasad Todkari <prasadtodkari@gmail.com>
2022-06-29 20:12:57 +0530

    Jun29

---
+++ Jun_29/index.js
@@ -0,0 +1,33 @@
+
+
+
+let myArr = [3, 5, 12, 76, 12];
+let i = 0;
+let count = myArr.length;
+
+/* let sum = 0;
+
+while (i < count){
+    sum = sum + myArr[i];
+    i = i + 1;
+}
+
+console.log(sum); */
+
+// Print Max
+// let arr = Math.max(27, 65, 87, 80, 122)
+// console.log(arr);
+
+// let num = [3, 52, 74,23, 8, 128];
+// let cnt = num.length;
+// let min = num[1];
+
+// for (let i = 0; i < cnt; i = i + 1){
+//     if(num[i]< min){
+//         min = num[i];
+//     }
+// }
+
+// console.log(min);
+
+let ar = [1, 2, 5, "somthing", false];
