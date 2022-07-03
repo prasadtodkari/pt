1404e5b00a515e843e0369e52d8a2acad1391fae
Prasad Todkari <prasadtodkari@gmail.com>
2022-07-03 18:30:45 +0530

    Task Completed for Jul 02

--- Jul_02/README.md
+++ Jul_02/README.md
@@ -1,14 +1,36 @@
 # Task of Jul 02
 
 ## Create 4 buttons and 4 functions (1 for each). 
-* Task 1 - Take an input from the user as a string and check whether it is a palindrome string or not.
-- (A palindrome string is a string which is same when read from left to right and right to left, example eye, radar, mom, kayak, etc etc...)
+* Task 1 - Take an input from the user as a string and check whether it is a palindrome string or not.**
+-> (A palindrome string is a string which is same when read from left to right and right to left, example eye, radar, mom, kayak, etc etc...)
+    
+# Tried with Built in Function
+    function palindrome() {
+    let input = prompt(); //Take Input from User.
+        console.log("User Input - " + input);
+
+    let inputArray = input.split(''); //I have Converted String to an array.
+        console.log("User Input Array - " + inputArray);
+
+    let reverseArray = inputArray.reverse(); //I have converted array in reverse direction.
+        console.log("converted Array - " + reverseArray);
+
+    let reverseinput = reverseArray.join(''); //And Reverse direction array converted to a string.
+        console.log("Input reversed - " + reverseinput);
+    
+    //Now Going to compare Inpot vs Reverse value of input.
+    if(input == reverseinput) {
+        alert('Your Input is palindrome');
+     } else {
+        alert('Your Input is not a palindrome');
+    }
+}
 
 * Task 2 - Take input from the user as a string and print the character that is most occured. 
-- (For example, from the Input ABCDAAA, the output will be A)/
+-> (For example, from the Input ABCDAAA, the output will be A)/
 
 * Task 3 - Take input fromm the user, convert it to a number and print the factorial of that number 
-- (For example, factorial of 5 = 5 * 4 * 3 * 2 * 1 = 120, print 120).
+-> (For example, factorial of 5 = 5 * 4 * 3 * 2 * 1 = 120, print 120).
 
 * Task 4 - Take input from the user as a string and print the number of characters of that string 
-- (excluding the vowels, For, example -> "Aayush" , output 3, "Unnati" -> 3).
+-> (excluding the vowels, For, example -> "Aayush" , output 3, "Unnati" -> 3).
--- Jul_02/task.js
+++ Jul_02/task.js
@@ -1,1 +1,96 @@
-// Task First
+// Task First Check palindrome text
+
+function palindrome() {
+    let input = prompt(); // Getting Input from User.
+    console.log(input); // verifying the Input.
+    
+    let reverseInput = ""; // to store string in reverse direction.
+    for (let i = input.length - 1; i >= 0; i = i - 1) {
+        reverseInput = reverseInput + input[i];
+    }
+    console.log(reverseInput); // verifying the Input is reversed correctly.
+
+    // Now I will compare the result vs input
+    if(input == reverseInput) {
+        console.log("Your Input is palindrome");
+        alert('Your Input is palindrome');
+     } else {
+        console.log("Your Input is not a palindrome");
+        alert('Your Input is not a palindrome');
+    }
+}
+
+
+// Task Second Check Maximum used character
+function maxchar(){
+    let string = prompt('Task 2 - Check Max occured character:');
+    console.log(string);
+    let char = {}; // store array of char with count.
+    let count = 0; // store count of char initial started with 0.
+    let result = ''; // store maximum used character in this variable.
+    for(let i of string){
+        char[i] = char[i] + 1 || 1;  
+    }
+    console.log(char);  
+    
+    //now comparing count with other and return result
+    for(let i in char){
+        if(char[i] > count){
+            count = char[i];
+            console.log(count);
+            result= i;
+            console.log(result);
+        }
+    }
+    alert(result);
+}
+
+// Task Third - Take input and provide factorial of that number
+function factorial(){
+    let num = prompt('For Task 3 please enter number');
+    let input = parseInt(num);
+    let result = input;
+        if(isNaN(input)){
+            alert("Characters are not allowed..!! (Numbers Only)");
+        }else{
+            for(let i = input - 1; i > 0; i = i - 1){
+                result = result * i;
+                console.log(result);
+            }
+            alert("Factorial of Your entered Number is =" + result);
+        }
+}
+
+// Task Forth - Count Non Wovels of String.
+function nonwovels(){
+     let input = prompt("For task 4 - Please provide your name OR string.");
+      console.log("Your entered text is - " + input); // verifying the input
+
+        if(isNaN(input)){ // verifying the user input
+            let count = 0;
+            for (var i = 0; i < input.length; i++) {
+            let single_character = input[i];
+            switch(single_character) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                count = count + 1;
+                break;
+            }
+            }
+            console.log('Count of Wovels - ' + count); // check the count of wovels
+
+            let nonwov = input.length - count; //total characters - wovels.
+            console.log('Number of Non Wovel characters : ' + nonwov);
+            alert("Number of Non Wovel characters are : " + nonwov);
+        } else {
+            alert("Numbers are not allowed. please Enter characters only");
+        }
+    }
