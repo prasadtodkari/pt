3302ef5b071731e8613cb5816d4e571cdff8cd48
Prasad Todkari <prasadtodkari@gmail.com>
2022-07-18 21:25:46 +0530

    Task 18 Jul 2022

---
+++ Jul_18/calc.js
@@ -0,0 +1,31 @@
+let display = document.getElementById('screen');
+let buttons = document.querySelectorAll('#btn');
+let dispValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+        if (buttonText == 'X') {
+            buttonText = '*'; //converted x to * for multiplication
+            dispValue += buttonText;
+            display.value = dispValue;
+        }
+        // When we click on C need to set display blank
+        else if (buttonText == 'C') {
+            dispValue = "";
+            display.value = dispValue;
+        }
+        // system default function to calculate clicked button value
+        else if (buttonText == '=') {
+            display.value = eval(dispValue);
+        }
+        // else display whatever is clicked by user on screen
+        else {
+            dispValue += buttonText;
+            display.value = dispValue;
+        }
+
+    })
+}
+
+
---
+++ Jul_18/index.js
@@ -0,0 +1,4 @@
+let btn = document.getElementById("btn");
+btn.addEventListener('click', function(){
+    alert("You have clicked button");
+})
